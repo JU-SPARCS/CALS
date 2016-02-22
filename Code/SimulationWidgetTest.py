3cf511d0c60e24760445254bb4e75cd3acc4530a
Augustin Tataru <tataru.augustin.gh@gmail.com>
2016-02-22 14:38:14 +0100

    More tests for the ui

--- Code/SimulationWidgetTest.py
+++ Code/SimulationWidgetTest.py
@@ -32,8 +32,68 @@ class SimulationWidgetTest(unittest.TestCase):
         self.assertEqual(self.widget.operational_cmbox.currentText(), "SC")
 
     def test_login_with_defaults(self):
-        # Should not pass, because the controller, facility and workstations IDs are not set
-        self.assertIsNone(self.widget.login())
+        self.assertFalse(self.widget.login())
+        self.assertFalse(self.widget.isLogged())
+
+    def test_login_normal(self):
+        self.widget.setLogged(False)
+        self.widget.controllerID_line.setText("420")
+        self.widget.facility_line.setText("420")
+        self.widget.workstationID_line.setText("420")
+
+        self.assertTrue(self.widget.login())
+        self.assertTrue(self.widget.isLogged())
+
+    def test_login_when_already_logged_in(self):
+        self.widget.setLogged(True)
+        self.widget.controllerID_line.setText("420")
+        self.widget.facility_line.setText("420")
+        self.widget.workstationID_line.setText("420")
+
+        self.assertFalse(self.widget.login())
+        self.assertTrue(self.widget.isLogged())
+
+        # test for a button click
+    def test_login_button_press(self):
+        self.widget.setLogged(False)
+        self.widget.controllerID_line.setText("420")
+        self.widget.facility_line.setText("420")
+        self.widget.workstationID_line.setText("420")
+
+        QTest.mouseClick(self.widget.login_btn, Qt.LeftButton)
+        self.assertTrue(self.widget.isLogged())
+
+    def test_logout_with_defaults(self):
+        self.assertFalse(self.widget.login())
+        self.assertFalse(self.widget.isLogged())
+
+    def test_logout_normal(self):
+        self.widget.setLogged(True)
+        self.widget.controllerID_line.setText("420")
+        self.widget.facility_line.setText("420")
+        self.widget.workstationID_line.setText("420")
+
+        self.assertTrue(self.widget.logout())
+        self.assertFalse(self.widget.isLogged())
+
+    def test_logout_when_not_logged_in(self):
+        self.widget.setLogged(False)
+        self.widget.controllerID_line.setText("420")
+        self.widget.facility_line.setText("420")
+        self.widget.workstationID_line.setText("420")
+
+        self.assertFalse(self.widget.logout())
+        self.assertFalse(self.widget.isLogged())
+
+
+    def test_role_change_normal(self):
+        self.widget.setLogged(True)
+        self.widget.controllerID_line.setText("420")
+        self.widget.facility_line.setText("420")
+        self.widget.workstationID_line.setText("420")
+
+        self.assertTrue(self.widget.rolechange())
+        self.assertTrue(self.widget.isLogged())
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(SimulationWidgetTest)
